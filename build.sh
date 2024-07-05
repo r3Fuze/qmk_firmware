8fc646402610a11bfe5341f287807c1183133c25
Morten Lindhardt <r3fuze@gmail.com>
2024-07-05 18:39:22 +0200

    Also copy keymap JSON to Windows qmk folder

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ qmk compile &&
 echo "Creating JSON file..." &&
 qmk c2json --no-cpp > r3fuze.json &&
 
-echo "Copying file to C:/Users/morten/Documents/qmk" &&
+echo "Copying files to C:/Users/morten/Documents/qmk" &&
 cp ~/qmk_firmware/input_club_whitefox_r3fuze.bin /mnt/c/Users/morten/Documents/qmk &&
+cp ~/qmk_firmware/r3fuze.json /mnt/c/Users/morten/Documents/qmk &&
 
 echo "Done"
