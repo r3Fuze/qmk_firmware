8459853a091e71a16aa76cf80f282f728846acae
Morten Lindhardt <r3fuze@gmail.com>
2025-03-28 18:54:23 +0100

    Change right control to context menu and right shift to right control

--- keyboards/input_club/whitefox/keymaps/r3fuze/keymap.c
+++ keyboards/input_club/whitefox/keymaps/r3fuze/keymap.c
@@ -123,9 +123,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐ Ent├───┤
      * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Æ │ Ø │ ' │    │Hom│
      * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┼───┤
-     * │Shft│ \ │ Z │ X │ C*│ V │ B │ N │ M*│ , │ . │ - │RShift│ ↑ │End│
+     * │Shft│ \ │ Z │ X │ C*│ V │ B │ N │ M*│ , │ . │ - │RCtrl │ ↑ │End│
      * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
-     * │Ctrl│Fn1 │Alt │                        │Alt│Win│Ctl│ ← │ ↓ │ → │
+     * │Ctrl│Fn1 │Alt │                        │Alt│Win│App│ ← │ ↓ │ → │
      * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      *  AltGr+C -> °
      *  AltGr+M -> μ
@@ -135,8 +135,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,     KC_1,       KC_2,       KC_3,   KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       DK_PLUS,    DK_ACUT,    KC_BSPC,    DK_HALF,
         KC_TAB,     KC_Q,       KC_W,       KC_E,   KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       DK_ARNG,    DK_DIAE,                KC_DEL,
         KC_CAPS,    KC_A,       KC_S,       KC_D,   KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       DK_AE,      DK_OSTR,    DK_QUOT,    KC_ENT,     KC_HOME,
-        KC_LSFT,    DK_LABK,    KC_Z,       KC_X,   KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     DK_MINS,    KC_RSFT,    KC_UP,      KC_END,
-        KC_LCTL,    MO(1),      KC_LALT,                                    KC_SPC,                             KC_RALT,    KC_LGUI,    KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT
+        KC_LSFT,    DK_LABK,    KC_Z,       KC_X,   KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     DK_MINS,    KC_RCTL,    KC_UP,      KC_END,
+        KC_LCTL,    MO(1),      KC_LALT,                                    KC_SPC,                             KC_RALT,    KC_LGUI,    KC_APP,     KC_LEFT,    KC_DOWN,    KC_RGHT
     ),
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
